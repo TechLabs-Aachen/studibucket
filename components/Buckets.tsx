45819a6e652dda64da19bf35cad69be92f6c8754
IliasOuz <iliasouzlim@gmail.com>
2023-07-27 18:54:50 +0200

    Bucket component finshed

---
+++ components/Buckets.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { StyleSheet, } from 'react-native';
+import { Card, Text } from 'react-native-paper';
+import { ProgressBar, MD3Colors } from 'react-native-paper';
+
+type BucketsProps = {
+    title: string,
+    date: Date,
+    goalAmount: number,
+    currentAmount: number,
+    active?: boolean,
+}
+
+const Buckets = (props: BucketsProps) => (
+  <Card>
+    <Card.Content style={[styles.bucket, props.active && styles.activeBucket]}>
+    <ProgressBar style={[styles.bar, props.active && styles.activeBar]} progress={props.active ? 1 - props.currentAmount/props.goalAmount : 1} color={props.active ? "white" : "#1D3557"} />
+      <Text style={[styles.textBucket, props.active && styles.activeTextBucket]} variant="titleLarge">{props.title}</Text>
+      <Text style={[styles.textBucket, props.active && styles.activeTextBucket]} variant="bodyMedium">{props.currentAmount} € {props.active? "until": "reached on"} {props.date.toLocaleDateString()}</Text>
+    </Card.Content>
+  </Card>
+);
+
+export default Buckets;
+
+const styles = StyleSheet.create({
+    activeBucket:{
+        backgroundColor: "#1D3557",
+    },
+    bucket:{
+        backgroundColor: "white",
+        borderRadius: 8,
+    },
+    textBucket:{
+        color: "#1D3557"
+    },
+    activeTextBucket:{
+        color: "white"
+    },
+    bar:{
+        backgroundColor: "grey"
+    },
+    activeBar:{
+        backgroundColor: "#E63946"
+    },
+}  
+)
--- pages/PlanningScreen.tsx
+++ pages/PlanningScreen.tsx
@@ -1,9 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Buckets from '../components/Buckets';
 
 
 export default function PlanningScreen() {
     return(
-        <Text>This is the Planing</Text>
+        <View style={styles.bucket}>
+            <Text>This is the Bucket</Text>
+            <Buckets title='Vacation' currentAmount={50.00} goalAmount={300.00} date={new Date()} active/>
+            <Buckets title='Car' currentAmount={5000.00} goalAmount={10000.00} date={new Date()} active/>
+            <Buckets title='Trip to bali' currentAmount={2000.00} goalAmount={2000.00} date={new Date()}/>
+        </View>
     );
 }
 
+const styles = StyleSheet.create({
+    bucket:{
+        flex: 1,
+        margin: 12,
+        gap: 15
+    }
+}  
+)

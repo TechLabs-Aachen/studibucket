c90cdae85df5e7818d189100a36b39f8fd5a0830
Mahmud <mahmuthepsev@gmail.com>
2023-08-14 17:11:43 +0200

    tabs cleaned

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -14,69 +14,81 @@ export default function MyTabs() {
   return (
     <View style={styles.container}>
       <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-      tabBar={({ navigation, state, descriptors, insets }) => (
-        <BottomNavigation.Bar
-          navigationState={state}
-         safeAreaInsets={insets}
-          onTabPress={({ route, preventDefault }) => {
-            const event = navigation.emit({
-              type: 'tabPress',
-              target: route.key,
-              canPreventDefault: true,
-            });
+        screenOptions={{
+          headerShown: false,
+        }}
+        tabBar={({ navigation, state, descriptors, insets }) => (
+          <BottomNavigation.Bar
+            navigationState={state}
+          safeAreaInsets={insets}
+            onTabPress={({ route, preventDefault }) => {
+              const event = navigation.emit({
+                type: 'tabPress',
+                target: route.key,
+                canPreventDefault: true,
+              });
 
-            if (event.defaultPrevented) {
-              preventDefault();
-            } else {
-             navigation.navigate(
-                route.name, route.params,
-              );
-            }
-          }}
-          renderIcon={({ route, focused, color }) => {
-            const { options } = descriptors[route.key];
-            if (options.tabBarIcon) {
-              return options.tabBarIcon({ focused, color, size: 24 });
-            }
+              if (event.defaultPrevented) {
+                preventDefault();
+              } else {
+              navigation.navigate(
+                  route.name, route.params,
+                );
+              }
+            }}
+            renderIcon={({ route, focused, color }) => {
+              const { options } = descriptors[route.key];
+              if (options.tabBarIcon) {
+                return options.tabBarIcon({ focused, color, size: 24 });
+              }
 
-            return null;
-          }}
-          getLabelText={({ route }) => {
-            return route.name;
-          }}
-        />
-      )}
-    >
+              return null;
+            }}
+            getLabelText={({ route }) => {
+              return route.name;
+            }}
+          />
+        )}
+      >
 
 
-      <Tab.Screen name="Home" component={() => <AppStack mainScreen={HomeScreen}/>} options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => {
-            return <Icon name="home" size={size} color={color} />;
-          },
-        }}/>
-        <Tab.Screen name="Cashflow" component={() => <AppStack mainScreen={CashflowScreen}/>} options={{
-          tabBarLabel: 'Cashflow',
-          tabBarIcon: ({ color, size }) => {
-            return <Icon name="cash-fast" size={size} color={color} />;
-          },
-        }}/>
-        <Tab.Screen name="Planning" component={() => <AppStack mainScreen={PlanningScreen}/>} options={{
-          tabBarLabel: 'Plannig',
-          tabBarIcon: ({ color, size }) => {
-            return <Icon name="calendar-month" size={size} color={color} />;
-          },
-        }}/>
-        <Tab.Screen name="Group" component={() => <AppStack mainScreen={GroupScreen}/>} options={{
-          tabBarLabel: 'Group',
-          tabBarIcon: ({ color, size }) => {
-            return <Icon name="account-group" size={size} color={color} />;
-          },
-        }}/>
-    </Tab.Navigator>       
+        <Tab.Screen 
+          name="Home" 
+          component={() => <AppStack mainScreen={HomeScreen}/>} 
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => {
+              return <Icon name="home" size={size} color={color} />;
+            },
+          }}/>
+        <Tab.Screen 
+          name="Cashflow" 
+          component={() => <AppStack mainScreen={CashflowScreen}/>} 
+          options={{
+            tabBarLabel: 'Cashflow',
+            tabBarIcon: ({ color, size }) => {
+              return <Icon name="cash-fast" size={size} color={color} />;
+            },
+          }}/>
+        <Tab.Screen 
+          name="Planning" 
+          component={() => <AppStack mainScreen={PlanningScreen}/>}   
+          options={{
+            tabBarLabel: 'Plannig',
+            tabBarIcon: ({ color, size }) => {
+              return <Icon name="calendar-month" size={size} color={color} />;
+            },
+          }}/>
+        <Tab.Screen 
+          name="Group" 
+          component={() => <AppStack mainScreen={GroupScreen}/>} 
+          options={{
+            tabBarLabel: 'Group',
+            tabBarIcon: ({ color, size }) => {
+              return <Icon name="account-group" size={size} color={color} />;
+            },
+          }}/>
+      </Tab.Navigator>       
     </View>   
   );
 }
